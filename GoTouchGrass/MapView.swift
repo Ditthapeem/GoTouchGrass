a21f37875302a9cb362d1014846ebefe4a5ca5b6
HuskyIsHere <lakagul.peem@gmail.com>
2024-05-11 01:09:33 +0700

    Update Map Pin

--- GoTouchGrass/MapView.swift
+++ GoTouchGrass/MapView.swift
@@ -15,24 +15,36 @@ struct IdentifiableAnnotation: Identifiable {
 }
 
 struct MapView: View {
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
     @State private var userLocation: CLLocationCoordinate2D?
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 13.8479, longitude: 100.5693), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)) // Default center location to Bangkok, Thailand
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 13.8479, longitude: 100.5693),
+        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    )
     @State private var identifiablePlaces: [IdentifiableAnnotation] = []
+    private var locationManager = CLLocationManager()
     
     var body: some View {
         NavigationView {
             ZStack {
-                Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow), annotationItems: identifiablePlaces) { identifiablePlace in
-                    MapPin(coordinate: identifiablePlace.annotation.coordinate, tint: .blue)
+                Map(
+                    coordinateRegion: $region,
+                    showsUserLocation: true,
+                    userTrackingMode: $userTrackingMode,
+                    annotationItems: identifiablePlaces
+                ) { identifiablePlace in
+                    MapMarker(
+                        coordinate: identifiablePlace.annotation.coordinate,
+                        tint: .blue
+                    )
                 }
                 .edgesIgnoringSafeArea(.all)
                 .onAppear {
-                    // Set initial user location and search for fitness places
+                    // Perform initial setup, like location request and data fetching
                     setUserLocation()
-                    searchForFitnessPlaces {
-                        print("places", identifiablePlaces)
-                    }
+                    searchForFitnessPlaces()
                 }
+
                 VStack {
                     Text("Gym Map")
                         .foregroundColor(Color(red: 35/255, green: 35/255, blue: 35/255))
@@ -44,56 +56,57 @@ struct MapView: View {
     }
     
     private func setUserLocation() {
-        // Request location authorization
-        let locationManager = CLLocationManager()
-        locationManager.requestWhenInUseAuthorization()
-        
-        // Fetch user's current location
         if let location = locationManager.location {
             userLocation = location.coordinate
             region.center = userLocation! // Update region to center on user's location
+        } else {
+            print("User location not available")
         }
     }
     
-    private func searchForFitnessPlaces(completion: @escaping () -> Void) {
-        guard let userLocation = userLocation else { return }
-        
-        // Clear previous results
+    private func searchForFitnessPlaces() {
+        guard let userLocation = userLocation else {
+            print("User location is nil.")
+            return
+        }
+
         identifiablePlaces.removeAll()
-        
+
         let request = MKLocalSearch.Request()
         request.naturalLanguageQuery = "fitness"
-        request.region = MKCoordinateRegion(center: userLocation, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
-        
+        request.region = MKCoordinateRegion(
+            center: userLocation,
+            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        )
+
         let search = MKLocalSearch(request: request)
         search.start { response, error in
-            guard let response = response else {
-                if let error = error {
-                    print("Error searching for fitness places: \(error.localizedDescription)")
-                } else {
-                    print("Unknown error searching for fitness places")
-                }
-                completion() // Call completion handler even in case of error
+            if let error = error {
+                print("Error searching for fitness places: \(error.localizedDescription)")
                 return
             }
-            
-            for item in response.mapItems {
+
+            guard let response = response else {
+                print("No response from the search.")
+                return
+            }
+
+            identifiablePlaces = response.mapItems.map { item in
                 let annotation = MKPointAnnotation()
                 annotation.coordinate = item.placemark.coordinate
                 annotation.title = item.name ?? "Fitness Place"
-                let identifiableAnnotation = IdentifiableAnnotation(annotation: annotation)
-                self.identifiablePlaces.append(identifiableAnnotation)
+                return IdentifiableAnnotation(annotation: annotation)
             }
-            
-            // Update the map's region to fit the annotations
-            if let firstPlace = self.identifiablePlaces.first?.annotation {
-                self.region = MKCoordinateRegion(center: firstPlace.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+
+            // If there are places, update the region to focus on them
+            if let firstPlace = identifiablePlaces.first {
+                region.center = firstPlace.annotation.coordinate
             }
-            
-            completion() // Call completion handler after places are populated
         }
     }
+
 }
+
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
